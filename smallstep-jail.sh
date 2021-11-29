52fd3b796a8fe99c947d11d92b4e88844b0f7633
Basil Hendroff <63370329+basilhendroff@users.noreply.github.com>
2021-11-30 00:00:03 +0800

    Update smallstep-jail.sh

--- smallstep-jail.sh
+++ smallstep-jail.sh
@@ -104,7 +104,7 @@ rm /tmp/pkg.json
 
 iocage exec "${JAIL_NAME}" mkdir -p /mnt/includes
 
-iocage fstab -a "${JAIL_NAME}" "${DATA_PATH}" /var/db/step_ca nullfs rw 0 0
+iocage fstab -a "${JAIL_NAME}" "${DATA_PATH}/db" /var/db/step_ca nullfs rw 0 0
 iocage fstab -a "${JAIL_NAME}" "${INCLUDES_PATH}" /mnt/includes nullfs rw 0 0
 
 # Copy pre-written config files
@@ -115,7 +115,7 @@ iocage exec "${JAIL_NAME}" sysrc step_ca_enable="YES"
 # Set up the STEPPATH environmental variable
 iocage exec "${JAIL_NAME}" echo "setenv STEPPATH /var/db/step_ca/ca" >> /etc/csh.cshrc
 
-iocage restart "${JAIL_NAME}"
+#iocage restart "${JAIL_NAME}"
 
 # Don't need /mnt/includes any more, so unmount it
 iocage fstab -r "${JAIL_NAME}" "${INCLUDES_PATH}" /mnt/includes nullfs rw 0 0
